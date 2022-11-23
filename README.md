b3df03c8414a60cdfd295680fdeafda5be114826
luckyian <ian.wren81@gmail.com>
2022-11-22 20:39:52 -0800

    Initial commit for framework to build app around

---
+++ README.md
@@ -0,0 +1,29 @@
+# Portfolio
+
+In this Portfolio I have to created multiple web pages that link together, and have their own content independent of the other pages.
+
+Each page has a navigation bar with links to the accompaning pages.  The navigation bar also has a drop down menu that contains the links when the size of the browser shinks.
+
+The index.html page has a biography about myself and a picture of me that is wrapped by the text in the biography.
+
+The contact.html page contains a form that can be filled and submitted.  The form contains a name input box, an e-mail input box, a comments/questions box, and a submit button.
+
+The portfolio.html page contains projects that I have worked on throughout my career.
+
+## Built with the following tech:
+
+*HTML
+*Bootstrap
+*CSS
+*Javascript
+*React
+*Github Pages
+*Bootstrap
+
+
+### Published link:
+https://luckyian.github.io/My_Portfolio/index.html
+
+#### Contributer:
+
+Ian Wren
---
+++ src/pages/Index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import "./style.css";
+
+
+function Index() {
+ 
+  
+  
+  return (
+
+
+    <div>
+      <div className="mt-5">
+        <div className="p-5">
+          <Card className="aboutBack">
+            <Card.Body>
+              <Card.Title><h1><u>About Me</u></h1></Card.Title>
+              <div className="float-left" >
+                <Card.Img src="./assets/Ian.jpg" className="p-2" alt="Ian Wren"></Card.Img>
+              </div>
+              <div>
+             <p><h4> My name is Ian Wren and I live in Seattle, WA.  I am 40 years old and
+                am looking for work as a front-end or full-stack developer.
+             <br></br>
+             <br></br>
+             I am looking to have a career as software developer with a focus on web
+             development. I currently work freelance for individuals and groups to design, build and deploy websites. My current projects include
+             a website for a baker in western Washington state, and developing projects with new technologies to keep my skills sharp.
+             <br></br>
+             <br></br>
+             I would like to work full-time for a software firm that allows me more time to spend with my loved ones, including my wife and
+             two feline fur-babies.
+             <br></br>
+             <br></br>
+             Some of my hobbies include playing video games, gardening, home improvement on my current
+             residence, hiking, trying new foods and drinks and travelling.  My youngest fur baby, Fox "Spooky" Mulder
+             enjoys going on walks or adventures around the neighborhood on his leash.
+             </h4></p>
+             <br></br>
+             <br></br>
+             <p>
+                  <h2><u>Contact Info</u></h2>
+                  <h3>Email: ian.wren81@gmail.com</h3>
+                  <h3>Phone: Contained in Resume</h3>
+                  <h3><a href="https://1drv.ms/b/s!AnD0ThTIlXlzg4EQTPFhLh06NqGOfQ?e=0oQU2u">Resume</a></h3>
+                  <h3>
+                    <a href="https://linkedin.com/in/ian-wren-53b382112">LinkedIn</a>
+                  </h3>
+                  <h3>
+                    <a href="https://github.com/luckyian">GitHub</a>
+                  </h3>
+
+              </p>
+
+            </div>
+
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+  );
+}
+
+export default Index;
