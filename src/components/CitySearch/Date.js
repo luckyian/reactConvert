53ee023d680e469904693fcfdb6bedaaf761efa8
luckyian <ian.wren81@gmail.com>
2022-11-30 12:55:55 -0800

    Added component to handle UNIX timestamp

---
+++ src/components/CitySearch/Date.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Date() {
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = month + ' ' + date + ' ' + year;
+    return time;
+
+}
+
+export default Date;
