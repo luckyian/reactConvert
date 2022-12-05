c392b2c0261dc9f83ff246e146bc1f46b98fb120
luckyian <ian.wren81@gmail.com>
2022-12-04 19:25:05 -0800

    Changed API calls to reflect new weather display data

--- src/components/CitySearch/WeatherDetail.js
+++ src/components/CitySearch/WeatherDetail.js
@@ -3,10 +3,10 @@ import React from "react";
 function WeatherDetail(props) {
   return (
     <div className="text-center">
-      <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
-      <h3>Director(s): {props.director}</h3>
-      <h3>Genre: {props.genre}</h3>
-      <h3>Released: {props.released}</h3>
+      <img alt={props.name} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
+      <h3>Date: {props.dt}</h3>
+      <h3>Humidity: {props.main.humidity}</h3>
+      <h3>Wind Speed: {props.wind.speed}</h3>
     </div>
   );
 }
