4def3cac781b213a0bf9b3284d516cb2cc9820b2
luckyian <ian.wren81@gmail.com>
2022-12-05 20:56:55 -0800

    Added text for weather display

--- src/components/CitySearch/WeatherDetail.js
+++ src/components/CitySearch/WeatherDetail.js
@@ -3,10 +3,10 @@ import React from "react";
 function WeatherDetail(props) {
   return (
     <div className="text-center">
-      <img alt={props.name} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
+      {/* <img alt={props.name} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} /> */}
       <h3>Date: {props.dt}</h3>
-      <h3>Humidity: {props.main.humidity}</h3>
-      <h3>Wind Speed: {props.wind.speed}</h3>
+      <h3>Humidity: {props.main.humidity} %</h3>
+      <h3>Wind Speed: {props.wind.speed} MPH</h3>
     </div>
   );
 }
